f80fdd157d5cd795ef00e1daadbf8534930822b8
benja83 <tarennebenjamin@gmail.com>
2015-09-26 12:20:27 +0200

    add movies routes using content controller

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::ContentsController < ApplicationController
   def index
-    contents = Content.order(:created_at)
+    @current_uri = request.env['PATH_INFO'].split('/').last.singularize.capitalize
+
+    contents = model_name.order(:created_at)
     render :json => contents
+  end
+
+  def model_names
+    ['Content', 'Movie']
+  end
+
+  def model_name
+    return @current_uri.constantize if @current_uri.in? model_names
   end
 end
--- spec/controllers/api/v1/contents_controller_spec.rb
+++ spec/controllers/api/v1/contents_controller_spec.rb
@@ -3,25 +3,65 @@ require 'rails_helper'
 RSpec.describe Api::V1::ContentsController, type: :controller do
 
   context 'index' do
-    before do
-      @contents = []
-      5.times { @contents << create(:movie) }
-      get :index
-    end
+    describe "using contents endpoint" do
+      before do
+        @contents = []
+        5.times do
+          @contents << create(:content)
+          @contents << create(:movie)
+        end
+        get :index
+      end
 
-    it 'returns the correct status' do
-      expect(response.status).to eql(200)
-    end
+      it 'returns the correct status' do
+        expect(response.status).to eql(200)
+      end
 
-    it 'returns the data in the body' do
-      body = MultiJson.load(response.body)
-      expect(response.body).to eql(@contents.to_json)
-    end
+      it 'returns the data in the body' do
+        body = MultiJson.load(response.body)
+        expect(response.body).to eql(@contents.to_json)
+      end
 
-    it "return the data in the order they are been created" do
-      body = MultiJson.load(response.body)
-      expect(response.body[0]).to eql(@contents.to_json[0])
+      it "return the data in the order they are been created" do
+        body = MultiJson.load(response.body)
+        expect(response.body[0]).to eql(@contents.to_json[0])
+      end
     end
   end
+# end
 
+# describe "Api::V1::ContentsController" do
+
+  context 'index' do
+    describe "using movie endpoint" do
+
+      before do
+
+        Rails.application.routes.draw do
+          get "api/v1/movie", :controller=>"contents"
+        end
+
+        @contents = []
+        5.times do
+          create(:content)
+          @contents << create(:movie)
+        end
+        get :api_v1_movie
+      end
+
+      it 'returns the correct status' do
+        expect(response.status).to eql(200)
+      end
+
+      it 'returns the data in the body' do
+        body = MultiJson.load(response.body)
+        expect(response.body).to eql(@contents.to_json)
+      end
+
+      it "return the data in the order they are been created" do
+        body = MultiJson.load(response.body)
+        expect(response.body[0]).to eql(@contents.to_json[0])
+      end
+    end
+  end
 end
