333feaecca11731555cb581e95d0f182d330307d
benja83 <tarennebenjamin@gmail.com>
2015-09-30 19:52:47 +0200

    adds association methods to user and contents

--- app/models/contents/content.rb
+++ app/models/contents/content.rb
@@ -1,5 +1,8 @@
 class Content < ActiveRecord::Base
   TYPES = %w( Movie Season )
+
+  has_many :purchases
+
   validates :title, :plot, :year, :price, presence: true
   validates :type, inclusion: { in: TYPES,
     message: "%{value} is not a valid type" }, :allow_nil => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :purchases
+
   validates :email, presence: true, uniqueness: true, email: true
 end
