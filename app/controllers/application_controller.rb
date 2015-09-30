31646087f2952e1145be1cde160a3b351f93092d
benja83 <tarennebenjamin@gmail.com>
2015-09-30 18:57:17 +0200

    adds current_user fake method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # faking current user to be the first user
+  def current_user
+    @currenr_user = User.first
+  end
 end
--- app/services/api/v1/purchase_service.rb
+++ app/services/api/v1/purchase_service.rb
@@ -1,6 +1,6 @@
-class Api::V1::PurchaseService
+class Api::V1::PurchaseService < ApplicationController
+
   def createPurchase(params, purchase_params)
-    @user = User.first
     purchase = Purchase.new new_params(params, purchase_params)
     if purchase.valid?
       purchase.save
@@ -10,6 +10,6 @@ class Api::V1::PurchaseService
   end
 
   def new_params(params, purchase_params)
-    purchase_params.merge(:user_id => @user.id, :content_id =>params[:content_id])
+    purchase_params.merge(:user_id => current_user.id, :content_id =>params[:content_id])
   end
 end
