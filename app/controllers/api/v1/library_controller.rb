05c674240564c89f4e33c883d49ba5279e6df659
benja83 <tarennebenjamin@gmail.com>
2015-10-01 01:13:49 +0200

    cleans unnecessary stuff

--- app/controllers/api/v1/library_controller.rb
+++ app/controllers/api/v1/library_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LibraryController < ApplicationController
   def index
     library = current_user.purchases.library.order(:expiration_date)
-    render json: library
+    render json: library, each_serializer: Api::V1::LibrarySerializer
   end
 end
--- app/serializers/api/v1/library_serializer.rb
+++ app/serializers/api/v1/library_serializer.rb
@@ -1,3 +1,3 @@
-class Api::V1::PurchaseSerializer < ActiveModel::Serializer
+class Api::V1::LibrarySerializer < ActiveModel::Serializer
   attributes :id, :quality, :expiration_date, :content_id
 end
