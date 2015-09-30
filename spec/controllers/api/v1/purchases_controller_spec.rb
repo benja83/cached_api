93c44fd4491a121fa6baf06f0e6e57711e8fb5e0
benja83 <tarennebenjamin@gmail.com>
2015-09-30 19:00:10 +0200

    adds more test for purchase

--- spec/controllers/api/v1/purchases_controller_spec.rb
+++ spec/controllers/api/v1/purchases_controller_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PurchasesController, type: :controller do
   describe "POST #create" do
-    context "with valid attributes" do
-
-      before do
+    before do
         create (:user)
         @contents = create_list(:content, 5) + create_list(:movie, 5) + create_list(:season, 5) << create(:content, year: 1900)
-      end
-
+    end
+    context "with valid attributes" do
       it "saves the new purchase in the database" do
         purchase_attributes = FactoryGirl.attributes_for(:purchase)
         expect{
@@ -17,17 +15,26 @@ RSpec.describe Api::V1::PurchasesController, type: :controller do
         }.to change(Purchase,:count).by(1)
       end
       it "return the purchase as response" do
-        purchase_attributes = FactoryGirl.attributes_for(:purchase, :invalid_purchase)
+        purchase_attributes = FactoryGirl.attributes_for(:purchase)
         post :create, { purchase: purchase_attributes,
                         content_id: @contents.first.id }
-        expect(response).to match_response_schema("purchaseError")
+        expect(response).to match_response_schema("purchase")
       end
     end
 
     context "with invalid attributes" do
       it "does not save the new purchase in the database" do
+        purchase_attributes = FactoryGirl.attributes_for(:purchase, :invalid_purchase)
+        expect{
+          post :create, { purchase: purchase_attributes,
+                          content_id: @contents.first.id }
+        }.to change(Purchase,:count).by(0)
       end
       it "return an error response" do
+        purchase_attributes = FactoryGirl.attributes_for(:purchase, :invalid_purchase)
+        post :create, { purchase: purchase_attributes,
+                        content_id: @contents.first.id }
+        expect(response).to match_response_schema("purchaseError")
       end
     end
   end
