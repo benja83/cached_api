27e5b1ac0c87e30cc1625d041bf34eb3046c24cb
benja83 <tarennebenjamin@gmail.com>
2015-09-30 19:31:57 +0200

    adds new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 YEAR_OPTION = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015]
-
+PRICE_OPTION = [0.99, 1.99, 2.99, 3.99, 4.99]
 
 User.create email: 'test@example.com'
 
-(1..5).each { |e|  Content.create plot: "Rambo#{e} is a nice film", title: "Rambo#{e}", year: YEAR_OPTION.sample }
-(1..5).each { |e|  Movie.create plot: "Rocki#{e} is a nice film", title: "Rocki#{e}", year: YEAR_OPTION.sample }
+(1..5).each { |e|  Content.create plot: "Rambo#{e} is a nice film", title: "Rambo#{e}", year: YEAR_OPTION.sample, price: PRICE_OPTION.sample }
+(1..5).each { |e|  Movie.create plot: "Rocki#{e} is a nice film", title: "Rocki#{e}", year: YEAR_OPTION.sample, price: PRICE_OPTION.sample }
 (1..rand(10)).each do |e|
   season = Season.create(
     plot: "Walking dead season #{e} is a nice serie",
     title: "Walking dead season#{e}",
-    year: YEAR_OPTION.sample
+    year: YEAR_OPTION.sample,
+    price: PRICE_OPTION.sample
   )
   (1..rand(10)).each do |n|
     episode = Episode.create(
