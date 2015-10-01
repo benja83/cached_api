b18ec9dabc5dbd875849c0608b6f3a007929d81f
benja83 <tarennebenjamin@gmail.com>
2015-10-01 18:25:21 +0200

    add caching for movies and seasons

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::MoviesController < ApplicationController
+
+  def initialize()
+    @service = Api::V1::CacheService.new(Api::V1::MovieSerializer)
+  end
   def index
     contents = Movie.order(:year)
+    @service.cache(contents)
     render :json => contents, each_serializer: Api::V1::MovieSerializer
   end
 end
--- app/controllers/api/v1/seasons_controller.rb
+++ app/controllers/api/v1/seasons_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::SeasonsController < ApplicationController
+  def initialize()
+    @service = Api::V1::CacheService.new(Api::V1::SeasonSerializer)
+  end
+
   def index
     contents = Season.includes(:episodes).order(:year)
+    @service.cache(contents)
     render :json => contents, each_serializer: Api::V1::SeasonSerializer
   end
 end
